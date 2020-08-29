38928728f52eff46d984b7b8b905051c7ac89872
DzabievaAA <DzabievaAA@gmail.com>
2020-08-29 17:14:04 +0300

    moved logic from circle element to game component

--- src/CircleElement/CircleElement.jsx
+++ src/CircleElement/CircleElement.jsx
@@ -1,20 +1,14 @@
 import React, {useState, useRef} from 'react';
 import styles from './CircleElement.module.css';
 
-const CircleElement = React.forwardRef ( ({color}, ref ) => {
+function CircleElement ({color, onClick, transform})  {
     
-    let [transform, setTransform] = useState(1)
-    function onClick (e) {
-        setTransform(1.6);
-        setTimeout(()=>{
-            setTransform(1)
-        }, 200)
-    }
+
     return <div>
-    <div onClick={onClick} ref={ref} id="circle" className={styles.circle}  style={{backgroundColor: color, transform: `scale(${transform}, ${transform})`}} >
+    <div onClick={onClick} className={styles.circle}  style={{backgroundColor: color, transform: transform}} >
     </div>
     
     </div>
-})
+}
 
 export default CircleElement;
--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -3,39 +3,24 @@ import CircleElement from '../CircleElement/CircleElement';
 import styles from '../CircleElement/CircleElement.module.css';
 
 function Game() {
-    const refRedCircle = useRef(null)
-    const refGreenCircle = useRef(null);
-    const refBlueCircle = useRef(null);
-    const refOrangeCircle = useRef(null);
-    let sequenceArray = []
-
-    let arrSize = 4;
+    let [sequenceArray, setSequenceArray] = useState ([]);
     let randomIntForCircle = 0;
+    let [transforms, setTransforms] = useState ([1, 1, 1, 1]);
+    
 
     function addElementToSequence(arrSize) {
      randomIntForCircle = Math.floor(Math.random() * Math.floor(arrSize));
-     console.log( randomIntForCircle );
-        if (randomIntForCircle === 0) {
-          sequenceArray.push(refRedCircle);
-        }
-        if (randomIntForCircle === 1) {
-          sequenceArray.push(refGreenCircle);
-       }
-        if (randomIntForCircle === 2) {
-          sequenceArray.push(refBlueCircle);
-      }
-        if (randomIntForCircle === 3) {
-          sequenceArray.push(refOrangeCircle);
-      }
+     sequenceArray.push(randomIntForCircle);
+     setSequenceArray(sequenceArray)
     }
 
     function playSequence () {
       let promise = new Promise((resolve)=>{
         resolve();
       });
-      for ( let element of sequenceArray ){
+      for ( let index of sequenceArray ){
         promise = promise.then(()=> new Promise((resolve) => setTimeout(() =>{
-                                                                element.current.click();
+                                                                playAnimationForCircle(index);
                                                                 resolve() 
                                                               }, 200
                                                             )
@@ -44,12 +29,23 @@ function Game() {
       }
     }
     
+    function playAnimationForCircle (index) {
+      let copyTransforms = [...transforms];
+      copyTransforms[index] = 1.6;
+      setTransforms(copyTransforms);
+      setTimeout(()=> {
+        let copyTransforms = [...transforms];
+        copyTransforms[index] = 1;
+        setTransforms(copyTransforms);
+      }, 200 )
+    }
+    
   return (
     <div className="Game">
-      <CircleElement ref={refRedCircle} color={"Red"}/>
-      <CircleElement ref={refGreenCircle} color={"Green"}/>
-      <CircleElement ref={refBlueCircle} color={"Blue"}/>
-      <CircleElement ref={refOrangeCircle} color={"Orange"}/>
+      <CircleElement  transform={`scale(${transforms[0]}, ${transforms[0]})`} onClick={() =>playAnimationForCircle(0)} color={"Red"}/>
+      <CircleElement  transform={`scale(${transforms[1]}, ${transforms[1]})`} onClick={() =>playAnimationForCircle(1)} color={"Green"}/>
+      <CircleElement  transform={`scale(${transforms[2]}, ${transforms[2]})`} onClick={() =>playAnimationForCircle(2)} color={"Blue"}/>
+      <CircleElement  transform={`scale(${transforms[3]}, ${transforms[3]})`} onClick={() =>playAnimationForCircle(3)} color={"Orange"}/>
       <button onClick={()=>{
         addElementToSequence(4)
         playSequence() }}>start game</button>
