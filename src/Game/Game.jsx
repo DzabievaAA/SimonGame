f6654f2a6db2b69ca7b1681b1ebdf59985c7ffa0
DzabievaAA <DzabievaAA@gmail.com>
2020-10-05 18:00:44 +0300

    style game. Description.

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useRef}from 'react';
 import CircleElement from '../CircleElement/CircleElement';
-import styles from '../CircleElement/CircleElement.module.css';
+import styles from './game.module.css';
 import Score from '../Score/Score';
+import information from '../img/information.png'
 
 function Game() {
     let [sequenceArray, setSequenceArray] = useState ([]);
@@ -11,6 +12,7 @@ function Game() {
     let [score, setScore] = useState(0);
     let [correctAnswers, setCorrectAnswers] = useState(0);
     let [gameMoving, setGameMoving] = useState(true);
+    let [gameMode, setGameMode] = useState("");
 
 
 
@@ -23,10 +25,16 @@ function Game() {
 
   let buttonVision;
   if(gameMoving === true) {
-    buttonVision = <button onClick={()=>{
-    addElementToSequence(4)
-    playSequence()
-    setGameMoving(false); }}>{textButton}</button>
+    buttonVision = 
+    <div className={styles.wrap}>
+      <button className={styles.button} onClick={()=>{
+        setGameMode('')
+        addElementToSequence(4)
+        playSequence()
+        setGameMoving(false); }}>
+          {textButton}
+      </button>
+    </div>
   }
   else {
     buttonVision = null;
@@ -69,10 +77,29 @@ function Game() {
       }, 200 )
     }
 
+    const [descriptionStyle, setDescriptionStyle] = React.useState( styles.description );
+
+    function myFunction() {
+      if (descriptionStyle === styles.description) {
+        setDescriptionStyle(styles.description_hidden);
+      } else {
+        setDescriptionStyle(styles.description);
+      }
+    }  
 
 
-  return (
-    <div className="Game">
+  return (<div>
+    <div href='' onClick={myFunction}>
+      <img className={styles.infoButton} src={information} wigth='40' height='40' alt=""/>
+    </div>
+    <span className={descriptionStyle}>WELCOME TO SIMON GAME<br></br><br></br>
+                                        The game creates a series of lights <br></br> 
+                                        and requires a user to repeat the sequence.<br></br>
+                                        If the user succeeds, the series <br></br>
+                                         becomes progressively longer <br></br> and more complex.Once the user fails,<br></br>
+                                         the game is over. 
+                                         </span>
+    <span className="Game">
       {["Red", "Green", "Blue", "Orange"].map((ColorName, index)=>{
         return <CircleElement  transform={`scale(${transforms[index]}, ${transforms[index]})`} 
         onClick={() =>{
@@ -83,17 +110,17 @@ function Game() {
           if(!animationPlaying){
             
             playAnimationForCircle(index)
-           
+      
             if (sequenceArray[correctAnswers] === index) {
               setCorrectAnswers(correctAnswers + 1);
               if (correctAnswers === sequenceArray.length -1){
-                alert("Level Completed");
+                setGameMode("Level Completed");
                 setScore(score + 1);
                 setCorrectAnswers(0);
                 setGameMoving(true);
               }
             } else {
-              alert("Game Over");
+              setGameMode("Game Over");
               setScore(0);
               setSequenceArray([]);
               setCorrectAnswers(0);
@@ -105,7 +132,12 @@ function Game() {
          } color={ColorName}/>
       })}
         {buttonVision}
-      <Score text={score}/>
+      <div className={styles.score}>
+        <Score  text={score}/>
+      </div>
+      <span>{gameMode}</span>
+    </span>
+    
     </div>
   );
 }
