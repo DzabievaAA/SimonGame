5b40fae9ceb23a5cb792d5100133e0c4a3e0725a
DzabievaAA <DzabievaAA@gmail.com>
2020-10-07 12:05:52 +0300

    Updates

---
+++ precache-manifest.443dc0e75f076867419e148a3950b313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91531310006552c643fae85869688036",
+    "url": "/SimonGame/index.html"
+  },
+  {
+    "revision": "0c6ad5933720982c7678",
+    "url": "/SimonGame/static/css/main.1e3e183f.chunk.css"
+  },
+  {
+    "revision": "3f33ddee1cd8017d1bbc",
+    "url": "/SimonGame/static/js/2.a1d1ed7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimonGame/static/js/2.a1d1ed7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6ad5933720982c7678",
+    "url": "/SimonGame/static/js/main.d7bb9002.chunk.js"
+  },
+  {
+    "revision": "fd9465c881bbefc73174",
+    "url": "/SimonGame/static/js/runtime-main.c0069961.js"
+  },
+  {
+    "revision": "3ae1c9013cdf848b0035b3d1c9b869d7",
+    "url": "/SimonGame/static/media/MiriamLibre-Regular.3ae1c901.ttf"
+  }
+]);
